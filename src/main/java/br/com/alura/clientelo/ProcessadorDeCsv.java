7423a6c500e426210491107ddb7a404f547100f8
victor.pongolino <victor.pongolino@hotmail.com>
2022-10-05 00:18:06 -0300

    FIX problema de nullos ao carregar um CSV

--- src/main/java/br/com/alura/clientelo/ProcessadorDeCsv.java
+++ src/main/java/br/com/alura/clientelo/ProcessadorDeCsv.java
@@ -7,7 +7,8 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ProcessadorDeCsv {
@@ -15,40 +16,34 @@ public class ProcessadorDeCsv {
     public static Pedido[] processaArquivo(String nomeDoArquivo) {
         try {
             URL recursoCSV = ClassLoader.getSystemResource(nomeDoArquivo);
-            Path caminhoDoArquivo = caminhoDoArquivo = Path.of(recursoCSV.toURI());
+            Path caminhoDoArquivo = Path.of(recursoCSV.toURI());
 
-            Scanner leitorDeLinhas = new Scanner(caminhoDoArquivo);
+            List<Pedido> pedidos = new ArrayList<>();
+            try (Scanner leitorDeLinhas = new Scanner(caminhoDoArquivo)) {
+            	leitorDeLinhas.nextLine();
 
-            leitorDeLinhas.nextLine();
-
-            Pedido[] pedidos = new Pedido[10];
-
-            int quantidadeDeRegistros = 0;
-            while (leitorDeLinhas.hasNextLine()) {
-                String linha = leitorDeLinhas.nextLine();
-                String[] registro = linha.split(",");
-
-                String categoria = registro[0];
-                String produto = registro[1];
-                BigDecimal preco = new BigDecimal(registro[2]);
-                int quantidade = Integer.parseInt(registro[3]);
-                LocalDate data = LocalDate.parse(registro[4], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                String cliente = registro[5];
-
-                Pedido pedido = new Pedido(categoria, produto, cliente, preco, quantidade, data);
-                pedidos[quantidadeDeRegistros] = pedido;
-
-                quantidadeDeRegistros++;
-                if (pedidos[pedidos.length - 1] != null) {
-                    pedidos = Arrays.copyOf(pedidos, pedidos.length * 2);
+                while (leitorDeLinhas.hasNextLine()) {
+                    String[] registro = leitorDeLinhas.nextLine().split(",");
+                    pedidos.add(from(registro));
                 }
             }
-
-            return pedidos;
+            
+            return pedidos.toArray(new Pedido[0]);
         } catch (URISyntaxException e) {
             throw new RuntimeException(String.format("Arquivo {} não localizado!", nomeDoArquivo));
         } catch (IOException e) {
             throw new RuntimeException("Erro ao abrir Scanner para processar arquivo!");
         }
+    }
+    
+    private static Pedido from(String[] registro) {
+    	String categoria = registro[0];
+        String produto = registro[1];
+        BigDecimal preco = new BigDecimal(registro[2]);
+        int quantidade = Integer.parseInt(registro[3]);
+        LocalDate data = LocalDate.parse(registro[4], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String cliente = registro[5];
+        
+        return new Pedido(categoria, produto, cliente, preco, quantidade, data);
     }
 }
